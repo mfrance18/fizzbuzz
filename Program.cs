d65c75523af118ca92f3e8d47472541432196d4b
Matt F <trumpetman072392@gmail.com>
2021-11-29 16:18:00 -0600

    init commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            List<string> list = new List<string>();
+
+
+
+            for (int n = 1; n <= 100; n++)
+            {
+                if (n % 15 == 0)
+                {
+                    list.Add("fizzbuzz");
+                }
+                else if (n % 3 == 0)
+                {
+                    list.Add("fizz");
+                }
+                else if (n % 5 == 0)
+                {
+                    list.Add("buzz");
+                }
+                else
+                {
+                    list.Add(n.ToString());
+                }
+            }
+
+            foreach (string s in list)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
